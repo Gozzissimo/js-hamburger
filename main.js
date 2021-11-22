0ecfe149b744b843cc52acfb5fedde9941012606
Gozzissimo <fantini.nico@gmail.com>
2021-11-22 16:18:28 +0100

    migliorati querySelecto

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 1. quando si clicca su "a.fas fa-bars" bisogna aggiungere la classe "active" ad hamburger menu
 
-const hamburgerButton = document.querySelector('.fa-bars');
+const hamburgerButton = document.querySelector('a .fa-bars');
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 
@@ -10,7 +10,7 @@ hamburgerButton.addEventListener('click', function () {
 
 // 2. quando si clicca su "a.close" bisogna togliere la classe active
 
-const closeButton = document.querySelector('.close');
+const closeButton = document.querySelector('a.close');
 
 closeButton.addEventListener('click', function () {
     hamburgerMenu.classList.remove('active');
