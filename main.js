0c699b90a868a3b33ced5788302365a842f64e88
Gozzissimo <fantini.nico@gmail.com>
2021-11-22 16:16:26 +0100

    Final

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 // 1. quando si clicca su "a.fas fa-bars" bisogna aggiungere la classe "active" ad hamburger menu
 
+const hamburgerButton = document.querySelector('.fa-bars');
 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerButton.addEventListener('click', function () {
+    hamburgerMenu.classList.add('active');
+});
 
 // 2. quando si clicca su "a.close" bisogna togliere la classe active
+
+const closeButton = document.querySelector('.close');
+
+closeButton.addEventListener('click', function () {
+    hamburgerMenu.classList.remove('active');
+});
